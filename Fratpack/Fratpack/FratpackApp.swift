9c589bbdbcca723b4c8372abd5f29d67765580ce
Jason Bodzy <jasonbodzy@gmail.com>
2022-05-08 21:56:39 -0500

    first commit

---
+++ Fratpack/Fratpack/FratpackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FratpackApp.swift
+//  Fratpack
+//
+//  Created by Jason Bodzy on 5/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FratpackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
